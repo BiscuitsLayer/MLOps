6672176246beb79aad58833af65567e875fe4cfe
BiscuitsLayer <vinogradov.so@phystech.edu>
2023-12-08 14:50:05 +0300

    Run script modified

--- Run.ps1
+++ Run.ps1
@@ -1,18 +1,31 @@
+# Auxiliary function to stop on failure
+function Invoke-Call {
+    param (
+        [scriptblock]$ScriptBlock,
+        [string]$ErrorAction = $ErrorActionPreference
+    )
+    & @ScriptBlock
+    if (($lastexitcode -ne 0) -and $ErrorAction -eq "Stop") {
+        Pop-Location
+        exit $lastexitcode
+    }
+}
+
 # Activate env
-python -m venv .
-./Scripts/Activate.ps1
+Invoke-Call -ScriptBlock { python -m venv . } -ErrorAction Stop
+Invoke-Call -ScriptBlock { ./Scripts/Activate.ps1 } -ErrorAction Stop
 
 # Install requirements
-poetry install
+Invoke-Call -ScriptBlock { poetry install } -ErrorAction Stop
 
 # Linter checks
-pre-commit install
-pre-commit run -a
+Invoke-Call -ScriptBlock { pre-commit install } -ErrorAction Stop
+Invoke-Call -ScriptBlock { pre-commit run -a } -ErrorAction Stop
 
 # Create new directories
-New-Item -ItemType Directory dataset -ErrorAction Ignore
-New-Item -ItemType Directory saved_model -ErrorAction Ignore
+Invoke-Call -ScriptBlock { New-Item -ItemType Directory dataset } -ErrorAction Ignore
+Invoke-Call -ScriptBlock { New-Item -ItemType Directory saved_model } -ErrorAction Ignore
 
 # Train and infer model
-python train.py
-python infer.py
+Invoke-Call -ScriptBlock { python train.py } -ErrorAction Stop
+Invoke-Call -ScriptBlock { python infer.py } -ErrorAction Stop
